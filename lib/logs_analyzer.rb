c17e948c7ee64cb0ba636f5a1b8920e6e5dbbfa4
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2020-10-05 17:11:18 +0100

    Initial commit.

---
+++ lib/logs_analyzer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'logs_analyzer/version'
+
+module LogsAnalyzer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/logs_analyzer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LogsAnalyzer do
+  it 'has a version number' do
+    expect(LogsAnalyzer::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
