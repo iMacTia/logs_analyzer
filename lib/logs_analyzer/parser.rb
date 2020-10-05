79b738c9fc3de30a470dab073b85cac70921f8c3
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2020-10-05 18:37:07 +0100

    LA-001 | Minor fixes to Parser

--- lib/logs_analyzer/parser.rb
+++ lib/logs_analyzer/parser.rb
@@ -11,37 +11,45 @@ module LogsAnalyzer
   # @example
   #   result = LogsAnalyzer::Parser.parse!('./file.log')
   class Parser
+    attr_reader :result, :file_path
+
     # Helper class method to save on the instantiation
     # @param file_path [String] the path to the file
+    # @return [Hash] the parsing result
     def self.parse!(file_path)
       new(file_path).parse!
     end
 
     # Initializes a new parser with a file path, without opening it yet
     # @param file_path [String] the path to the file
+    # @raise [ArgumentError] if file_path is not a String
     def initialize(file_path)
       raise ArgumentError, 'Argument `file_path` must be a String.' unless file_path.is_a?(String)
 
       @file_path = file_path
       @parsed = false
-      @res = {}
+      @result = {}
     end
 
+    # Checks if parse! has been called
+    # @return [Boolean] true if file has been parsed, false otherwise
     def parsed?
       @parsed
     end
 
+    # Parses the file and returns the result
+    # @return [Hash] the parsing result
+    # @raise [LogsAnalyzer::ParseError] if an invalid line is found while parsing
     def parse!
+      # Uses `File.foreach` to reduce memory usage
       File.foreach(file_path).with_index do |line, index|
         parse_line(line, index)
       end
       @parsed = true
-      res
+      result
     end
 
     private
-
-    attr_reader :res, :file_path
 
     def parse_line(line, index)
       uri, ip = line.split(' ')
@@ -49,7 +57,7 @@ module LogsAnalyzer
       raise LogsAnalyzer::ParseError, "Invalid line: #{index}." unless uri && ip
 
       # ensures uri is already initialized in result
-      res[uri] ||= {
+      result[uri] ||= {
         total: 0,
         unique: 0
       }
@@ -58,13 +66,13 @@ module LogsAnalyzer
     end
 
     def add_visit_to_result(uri, ip)
-      if res[uri][ip]
-        res[uri][ip] += 1
+      if result[uri][ip]
+        result[uri][ip] += 1
       else
-        res[uri][ip] = 1
-        res[uri][:unique] += 1
+        result[uri][ip] = 1
+        result[uri][:unique] += 1
       end
-      res[uri][:total] += 1
+      result[uri][:total] += 1
     end
   end
 end
