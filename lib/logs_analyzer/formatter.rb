0af46340ca3327d3ca4566b59c02142f463fc344
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2020-10-05 19:01:54 +0100

    LA-001 | Abandon CSV format in favour of more generic list for Formatter implementation and fixes tests

--- lib/logs_analyzer/formatter.rb
+++ lib/logs_analyzer/formatter.rb
@@ -15,22 +15,22 @@ module LogsAnalyzer
       @parsed = parsed
     end
 
-    # Returns a report with the total visits per page, in CSV format
-    # @return [String] the report in CSV format
+    # Returns a report with the total visits per page
+    # @return [String] the report
     def total_report
-      ''
+      format_visits(:total).join("\n")
     end
 
-    # Returns a report with the total visits per page, in CSV format
-    # @return [String] the report in CSV format
+    # Returns a report with the total visits per page
+    # @return [String] the report
     def unique_report
-      ''
+      format_visits(:unique).join("\n")
     end
 
     private
 
     def format_visits(key)
-      ''
+      @parsed.sort_by { |_, v| -v[key] }.map { |k, v| "#{k}: #{v[key]}" }
     end
   end
 end
--- spec/logs_analyzer/formatter_spec.rb
+++ spec/logs_analyzer/formatter_spec.rb
@@ -9,15 +9,15 @@ RSpec.describe LogsAnalyzer::Formatter do
     subject { formatter.total_report }
 
     it 'returns total visits report for all pages' do
-      expect(subject.split("\n").size).to eq(7) # 6 rows + headers
+      expect(subject.split("\n").size).to eq(6)
     end
 
     it 'returns the most visited page at the top' do
-      is_expected.to start_with('/about/2,90')
+      is_expected.to start_with('/about/2: 90')
     end
 
     it 'returns the least visited page at the bottom' do
-      is_expected.to end_with('/home,78')
+      is_expected.to end_with('/home: 78')
     end
   end
 
@@ -25,15 +25,15 @@ RSpec.describe LogsAnalyzer::Formatter do
     subject { formatter.unique_report }
 
     it 'returns unique visits report for all pages' do
-      expect(subject.split("\n").size).to eq(7) # 6 rows + headers
+      expect(subject.split("\n").size).to eq(6)
     end
 
     it 'returns the most uniquely visited page at the top' do
-      is_expected.to start_with('/help_page/1,23')
+      is_expected.to start_with('/help_page/1: 23')
     end
 
     it 'returns the least uniquely visited page at the bottom' do
-      is_expected.to end_with('/about,21')
+      is_expected.to end_with('/about: 21')
     end
   end
 end
